905b41b179e80dfacb01d525d868b17b3d52881b
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-04-18 23:31:43 +0900

    Add files via upload

--- clone/src/components/Home.js
+++ clone/src/components/Home.js
@@ -5,23 +5,19 @@ import { useNavigate } from "react-router-dom";
 function Home() {
     return (
         <StyledTop>
-            <button style={homebtn}>
-
-                안녕하세요?
-
-            </button>
+                메인
         </StyledTop>
     );
 };
 
 export default Home;
 
-const StyledTop = Styled.h1`
+const StyledTop = Styled.button`
     text-align: center;
 `;
 
-const homebtn = {
-    color: "black",
-    padding: "1rem",
-    border: "2px solid black"
-}
+// const homebtn = {
+//     color: "black",
+//     padding: "1rem",
+//     border: "2px solid black"
+// }
