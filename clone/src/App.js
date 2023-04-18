0e395b363357e52de9dc7bbd1b0b82cbc0af498d
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-04-19 00:56:14 +0900

    Add files via upload

--- clone/src/App.js
+++ clone/src/App.js
@@ -2,13 +2,14 @@ import React from "react"
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import First from "./components/First";
-
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
-      <Home />
       <Router>
+        <Home />
+        <Search />
         <Routes>
           <Route path="/first" element={<First />} />
         </Routes>
--- clone/src/components/Home.js
+++ clone/src/components/Home.js
@@ -3,21 +3,35 @@ import Styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate("/");
+    };
+
     return (
         <StyledTop>
-                메인
+            <button
+            style={homebtn}
+            onClick={handleButtonClick}
+            >
+                화면 로고 이미지 넣기
+            </button>
         </StyledTop>
     );
 };
 
 export default Home;
 
-const StyledTop = Styled.button`
-    text-align: center;
+const StyledTop = Styled.div`
+    /* text-align: center; */
 `;
 
-// const homebtn = {
-//     color: "black",
-//     padding: "1rem",
-//     border: "2px solid black"
-// }
+const homebtn = {
+    color: "black",
+    padding: "1rem",
+    border: "2px solid black",
+    cursor: "pointer",
+    marginLeft: "5rem",
+    marginTop: "3rem"
+}
