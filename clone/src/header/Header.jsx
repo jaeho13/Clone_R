610e357d09e5cd71b9bdf6d3157216db5a4cc90e
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-05-02 15:59:55 +0900

    Add files via upload

--- clone/src/header/Header.jsx
+++ clone/src/header/Header.jsx
@@ -5,10 +5,12 @@ import Styled from "styled-components"
 const Header = props => {
     return (
         <Top>
-            <div>사진</div>            
+            <Div>사진</Div>
             <Input type="text" placeholder="검색하기" />
-            <div>메세지</div>
-            <div>로그아웃</div>
+            <ButtonGroup>
+                <Button>메세지</Button>
+                <Button>로그아웃</Button>
+            </ButtonGroup>
         </Top>
     )
 };
@@ -23,11 +25,23 @@ const Top = Styled.header`
     display: flex;
     flex-direction: row;
     justify-content: space-around;
-    align-items: center
-`;
+    align-items: center;
+`
+const Div = Styled.div`
+    
+`
 
 const Input = Styled.input`
   width: 40%;
-  height: 40px;
-  
+  height: 40px; 
+`
+const ButtonGroup = Styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
 `;
+
+const Button = Styled.button`
+  width: 100px;
+  height: 50px;
+`
